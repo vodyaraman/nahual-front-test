d7191cbda084e6e39151d4d35674badd58e6c49c
vodyaraman <130937079+vodyaraman@users.noreply.github.com>
2024-11-03 17:11:49 +0300

    Build error fix 2

--- src/components/layouts/StateLayout.tsx
+++ src/components/layouts/StateLayout.tsx
@@ -1,26 +1,24 @@
 // src/components/layouts/AppLayout.tsx
 
-import React, { ReactNode, Suspense } from 'react';
+import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@/state/store';
 import { CssBaseline } from '@mui/material';
 import StyleLayout from './StyleLayout';
 
 interface StateLayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const StateLayout: React.FC<StateLayoutProps> = ({ children }) => {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Provider store={store}>
-                <StyleLayout>
-                    <CssBaseline />
-                    {children}
-                </StyleLayout>
-            </Provider>
-        </Suspense>
-    );
+  return (
+    <Provider store={store}>
+      <StyleLayout>
+        <CssBaseline />
+        {children}
+      </StyleLayout>
+    </Provider>
+  );
 };
 
 export default StateLayout;

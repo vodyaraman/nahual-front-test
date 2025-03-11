e223d527690045036c63e57d39525136cf613295
Pawlord <Pavel23082002@mail.ru>
2025-03-11 22:35:32 +0300

    Start work on animation of About block

---
+++ src/components/common/about-work-step/AboutWorkStep.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useRef } from 'react';
+
+// Стили
+import './AboutWorkStep.scss';
+
+// Компоненты
+import Image from 'next/image';
+
+// GSAP
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+type Props = {
+    imgUrl: string,
+    title: string,
+    text: string,
+    index: number,
+}
+
+export default function AboutWorkStep({ imgUrl, title, text, index }: Props) {
+    gsap.registerPlugin(ScrollTrigger);
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.about-work__steps',
+                start: 'top center',
+                end: 'bottom 40%',
+                markers: {
+                    startColor: 'white',
+                    endColor: 'white',
+                }
+            }
+        })
+
+        tl
+            .from('.step', { x: -100, opacity: 0.2 })
+            .to('.step', { x: 0, opacity: 1 })
+    })
+
+    return (
+
+        <div className="step">
+            <figure className="step__desc">
+                <Image className="step-img" src={imgUrl} alt="step-1" width={250} height={250} />
+                <figcaption>
+                    <p className="step__desc-title">
+                        {title}
+                    </p>
+                    <p className="step__desc-text text-right">
+                        {text}
+                    </p>
+                </figcaption>
+            </figure>
+        </div>
+
+    )
+}
